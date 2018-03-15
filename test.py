3866ad07d6ddc07512c8f1304fe8da63486e98b1
zhengyu14 <jan25th@live.cn>
2018-03-15 17:43:18 +0800

    Learning Data Mining with Python: Ch1
    Finished

--- test.py
+++ test.py
@@ -1,57 +1,18 @@
 # Path (Mac): /Users/Zhengyu/Documents/Git/PythonStudy/test.py
 # Path (Win): C:\Users\i323429\Documents\GitHub\PythonStudy\test.py
-class coordinatePoint:
-    xAxis = 0.0
-    yAxis = 0.0
+from sklearn.datasets import load_iris
+import numpy as np
+from collections import defaultdict
+from operator import itemgetter
+dataset = load_iris()
+iris_data = dataset.data
+iris_target = dataset.target
+#print (iris_data.shape[1])
 
-    def __init__(self, xAxis, yAxis):
-        self.xAxis = xAxis
-        self.yAxis = yAxis
-
-class cluster:
-    dataPoints = []
-
-    # Initialize a cluster with a centroid
-    def __init__(self, centroidXAxis, centroidYAxis):
-        self.centroid = coordinatePoint(centroidXAxis, centroidYAxis)
-
-    def addToCluster(self, dataPoint):
-        self.dataPoints.append(dataPoint)
-
-    def clusterVol(self):
-        return len(self.dataPoints)
-
-    def clearCluster(self):
-        del self.dataPoints[:]
-
-    def updateCentroid(self):
-        xAxisSum = 0.0
-        yAxisSum = 0.0
-
-        for index in range(len(self.dataPoints)):
-            xAxisSum += self.dataPoints[index].xAxis
-            yAxisSum += self.dataPoints[index].yAxis
-
-        self.centroid.xAxis = xAxisSum / len(self.dataPoints)
-        self.centroid.yAxis = yAxisSum / len(self.dataPoints)
-
-testCluster = cluster(0, 0)
-testCluster.addToCluster(coordinatePoint(1, 0))
-testCluster.addToCluster(coordinatePoint(0, 2))
-testCluster.addToCluster(coordinatePoint(6, 4))
-
-print "Data in cluster: "
-
-for index in range(len(testCluster.dataPoints)):
-    print "x: ", testCluster.dataPoints[index].xAxis, "y: ", testCluster.dataPoints[index].yAxis
-
-print "New centroid: "
-
-testCluster.updateCentroid()
-
-print "x: ", testCluster.centroid.xAxis, "y: ", testCluster.centroid.yAxis
-
-print testCluster.clusterVol()
-
-testCluster.clearCluster()
-print len(testCluster.dataPoints)
+class_counts = defaultdict(int)
+class_counts[0] = 2
+class_counts[1] = 1
+class_counts[2] = 0
+print ([class_count for class_value, class_count in class_counts.items() if class_value != 0])
+error = sum([class_count for class_value, class_count in class_counts.items() if class_value != 0])
+print(error)
