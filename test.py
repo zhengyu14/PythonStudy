07c90337729f7a5f2c50757d08d814c703b0855d
zhengyu14 <jan25th@live.cn>
2018-03-16 17:23:56 +0800

    -

--- test.py
+++ test.py
@@ -1,18 +1,12 @@
 # Path (Mac): /Users/Zhengyu/Documents/Git/PythonStudy/test.py
 # Path (Win): C:\Users\i323429\Documents\GitHub\PythonStudy\test.py
-from sklearn.datasets import load_iris
-import numpy as np
-from collections import defaultdict
-from operator import itemgetter
-dataset = load_iris()
-iris_data = dataset.data
-iris_target = dataset.target
-#print (iris_data.shape[1])
 
-class_counts = defaultdict(int)
-class_counts[0] = 2
-class_counts[1] = 1
-class_counts[2] = 0
-print ([class_count for class_value, class_count in class_counts.items() if class_value != 0])
-error = sum([class_count for class_value, class_count in class_counts.items() if class_value != 0])
-print(error)
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-1, 1, 50)
+y = 2*x + 1
+
+plt.figure()
+plt.plot(x, y)
+plt.show()
